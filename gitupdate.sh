799a90dea52cdb93fe5c7205f62a63f099fec57a
Glittering <zhaoshichengzhao@sina.com>
2017-04-05 12:16:55 +0800

    git

---
+++ gitupdate.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/bash
+
+array_name=(
+bashion_ktv 
+netEase 
+pythonLearn 
+);
+
+bash_str=~/Documents/
+bash_log=log.md
+
+for i in ${array_name[@]};
+do
+    log=$bash_str$bash_log
+    use_str=$bash_str$i
+    date >> $log
+    echo '###'$use_str >> $log
+    echo '-------------NOW IS :--------------'
+    pwd
+    cd $use_str
+    git add *  > /dev/null
+    git commit -m 'sudo commit' > /dev/null
+    git status >> $log
+    git push origin master >> $log
+    git status >> $log
+    echo '---------' >> $log
+    echo '' >> $log
+done
+
